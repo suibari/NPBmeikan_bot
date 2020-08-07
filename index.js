6eb1807db1b0299dc614e6784d12cd121b0d62e2
suibari <404.not.found.address@gmail.com>
2020-08-07 16:50:37 +0900

    bug fix

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ function handleEvent(event) {
         messages = [
                     {
                       type: 'template',
+                      altText: "このデバイスは複数選手検索に被対応です。",
                       template: {
                         type: 'carousel',
                         columns: []
