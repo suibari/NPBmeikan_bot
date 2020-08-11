3f173e3fa50a78800a8eecbc93bfaddfa8a381a7
suibari <404.not.found.address@gmail.com>
2020-08-12 00:03:41 +0900

    add a function search by teamname and player number.

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const pool     = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
-const query    = `SELECT * FROM player WHERE data->>'name' LIKE '%' || $1 || '%' ;`;
+const query_name    = `SELECT * FROM player WHERE data->>'name' LIKE '%' || $1 || '%' ;`;
+const query_team_no = `SELECT * FROM player WHERE data->>'team' = '$1' AND data->>'no' = '$2';`;
+//const query    = `SELECT * FROM player WHERE data->>'name' SIMILAR TO '%' || $1 || '%' ;`
 
 // create LINE SDK config from env variables
 const config = {
@@ -40,9 +42,33 @@ function handleEvent(event) {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
+  
+  // SQL-selet (チーム名&背番号検索)
+  var dct_tn = detectTeamAndNum(event.message.text);
+  if ((dct_tn.team) && (dct_tn.num)) {
+    pool.query(query_team_no, [dct_tn.team, dct_tn.num])
+    .then((res) => {
+      var messages;
+      const message = arrangeText(res.rows[0].data);
+      messages = [
+        { 
+          type: 'image', 
+          originalContentUrl: res.rows[0].data.photo_url,
+          previewImageUrl: res.rows[0].data.photo_url
+        },
+        { 
+          type: 'text',
+          text: message
+        }
+      ];
 
-  // SQL-select
-  pool.query(query, [event.message.text])
+      return client.replyMessage(event.replyToken, messages);
+    })
+    .catch(err => console.error('Error executing query', err.stack));
+  }
+
+  // SQL-select (選手名検索)
+  pool.query(query_name, [event.message.text])
   .then((res) => {
     var messages;
 
@@ -55,7 +81,6 @@ function handleEvent(event) {
 
       if (msg_length <= 5) {
         // 検索結果が50人以下なので、LINEで表示可能
-
         // 選手数を10で割った商+1 分、messages配列にオブジェクトを作って格納する
         for (let j=0; j<msg_length; j++) {
           messages[j] = {
@@ -76,18 +101,6 @@ function handleEvent(event) {
             // 現在が最後でない
             i_max = 10;
           };
-          //var i = 0;
-          //do {
-          //  messages[j].template.columns[i]       = {};
-          //  messages[j].template.columns[i].title = res.rows[j*10+i].data.name;
-          //  messages[j].template.columns[i].text  = res.rows[j*10+i].data.team;
-          //  messages[j].template.columns[i].actions          = [{}];
-          //  messages[j].template.columns[i].actions[0].type  = "message";
-          //  messages[j].template.columns[i].actions[0].label = "この選手を検索";
-          //  messages[j].template.columns[i].actions[0].text  = res.rows[j*10+i].data.name;
-          //  messages[j].template.columns[i].defaultAction    = messages[j].template.columns[i].actions[0];
-          //  i += i;
-          //} while (i < i_max);
 
           for (let i=0; i<i_max; i++) {
             messages[j].template.columns[i]       = {};
@@ -131,6 +144,7 @@ function handleEvent(event) {
     // use reply API
     return client.replyMessage(event.replyToken, messages);
   })
+  .catch(err => console.error('Error executing query', err.stack));
 
   // old: 直接スクレイピングをしていたやり方
   //require('./scraping.js').getPlayerData(event.message.text).then( obj => {
@@ -207,7 +221,7 @@ app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
 
-// LINE表示用にテキスト整形
+// LINE表示用にテキスト整形する関数
 function arrangeText(obj) {
   var res;
 
@@ -231,3 +245,47 @@ function arrangeText(obj) {
   }
   return res;
 }
+
+// テキストにチーム名、背番号が含まれるか判定する関数
+function detectTeamAndNum(text) {
+  const teams = {
+    g: ["読売ジャイアンツ", "読売", "巨人", "ジャイアンツ"],
+    b: ["横浜DeNAベイスターズ", "横浜", "DeNA", "ベイスターズ"],
+    t: ["阪神タイガース", "阪神", "タイガース"],
+    c: ["広島東洋カープ", "広島", "カープ"],
+    d: ["中日ドラゴンズ", "中日", "ドラゴンズ"],
+    s: ["東京ヤクルトスワローズ", "ヤクルト", "スワローズ"],
+    l: ["埼玉西武ライオンズ", "西武", "ライオンズ"],
+    h: ["福岡ソフトバンクホークス", "ホークス", "ソフトバンク"],
+    e: ["東北楽天ゴールデンイーグルス", "楽天", "イーグルス"],
+    m: ["千葉ロッテマリーンズ", "ロッテ", "マリーンズ"],
+    f: ["北海道日本ハムファイターズ", "日ハム", "ファイターズ"],
+    bf: ["オリックス・バファローズ", "オリックス", "バファローズ", "バッファローズ"]
+  };
+  
+  var result = {
+    team = null,
+    num  = null
+  };
+  for (key in teams) result.team = checkInclude(text, teams[key]); // textにチーム名が入っているか判定
+  result.num = detectNum(text);                                    // textに数値が入っているか判定
+  return result;
+
+  // textに背番号が含まれているか判定し、含まれていたら背番号を返す関数
+  function detectNum(text) {
+    // 全角数字を半角に直す
+    text = text.replace(/[０-９]/g, s => String.fromCharCode(s.charCodeAt(0) - 65248));
+    num  = text.replace(/[^0-9]/g, "");
+    return num;
+  };
+
+  // textがチーム名配列に含まれているか判定し、含まれていたらチーム名を返す関数
+  function checkInclude(text, matchArary) {
+    matchArary.some(function(key){
+      if(text.match(key)) {
+        inc_team = matchArary[0];
+        return inc_team;
+      }
+    });
+  };
+};
