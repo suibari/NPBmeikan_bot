fe028911bb164344ae211a5fb004498a30ce213e
suibari <404.not.found.address@gmail.com>
2020-08-12 23:21:55 +0900

    referctaring

--- index.js
+++ index.js
@@ -51,23 +51,12 @@ function handleEvent(event) {
     pool.query(query_team_no, [dct_tn.team, dct_tn.num])
     .then((res) => {
       if (res.rowCount > 0) {
-        const message = arrangeText(res.rows[0].data);
-        messages = [
-          { 
-            type: 'image', 
-            originalContentUrl: res.rows[0].data.photo_url,
-            previewImageUrl: res.rows[0].data.photo_url
-          },
-          { 
-            type: 'text',
-            text: message
-          }
-        ];
+        messages = createMsgObj(res.rows[0].data);
       } else {
         // 選手hitしなかった場合
         messages = {
           type: 'text',
-          text: "選手が見つかりませんでした。"
+          text: dct_tn.team + "の背番号" + dct_tn.num + "はいませんでした。"
         };
       }
       // use reply API
@@ -130,18 +119,7 @@ function handleEvent(event) {
         };
       } else if (res.rowCount == 1) {
         // 単一選手hitした場合
-        const message = arrangeText(res.rows[0].data);
-        messages = [
-          { 
-            type: 'image', 
-            originalContentUrl: res.rows[0].data.photo_url,
-            previewImageUrl: res.rows[0].data.photo_url
-          },
-          { 
-            type: 'text',
-            text: message
-          }
-        ];
+        messages = createMsgObj(res.rows[0].data);
       } else {
         // 選手hitしなかった場合
         messages = {
@@ -155,74 +133,6 @@ function handleEvent(event) {
     .catch(err => console.error('Error executing query', err.stack));
   }
 }
-
-  // old: 直接スクレイピングをしていたやり方
-  //require('./scraping.js').getPlayerData(event.message.text).then( obj => {
-  //  var messages;
-  //
-  //  if (obj) {
-  //    if (Array.isArray(obj)) {
-  //      // 複数選手検索結果(Array)が返ってきた場合
-  //      messages = [];
-  //      const msg_length  = Math.ceil(obj.length / 10); // 選手数を10で割った商+1を計算
-  //      const lastmsg_num = obj.length - Math.floor(obj.length / 10) * 10; // 選手数を10で割った余りを計算
-  //      var i_max;
-  //
-  //      if (msg_length <= 5) {
-  //        // 検索結果が50人以下なので、LINEで表示可能
-  //
-  //        // 選手数を10で割った商+1 分、messages配列にオブジェクトを作って格納する
-  //        for (let j=0; j<msg_length; j++) {
-  //          messages[j] = {
-  //            type: 'template',
-  //            altText: "複数選手検索結果",
-  //            template: {
-  //              type: 'carousel',
-  //              columns: []
-  //            }
-  //          };
-  //          if (j == msg_length-1) { // 現在作成してるオブジェクトが最後かどうか
-  //            i_max = lastmsg_num;
-  //          } else {
-  //            i_max = 10;
-  //          };
-  //          for (let i=0; i<i_max; i++) {
-  //            messages[j].template.columns[i]       = {}
-  //            messages[j].template.columns[i].title = obj[j*10+i].name;
-  //            messages[j].template.columns[i].text  = obj[j*10+i].team;
-  //            messages[j].template.columns[i].actions          = [{}];
-  //            messages[j].template.columns[i].actions[0].type  = "message";
-  //            messages[j].template.columns[i].actions[0].label = "この選手を検索";
-  //            messages[j].template.columns[i].actions[0].text  = obj[j*10+i].name;
-  //          }
-  //        };
-  //        //console.log(messages);
-  //      } else {
-  //        // 検索結果が50人より大きいので、LINEで表示不可。エラーを返す
-  //        messages = {type: 'text',
-  //                    text: "選手検索結果が50件を超えました。もう少し長い選手名で試してみてください。"};
-  //      };
-  //      
-  //    } else {
-  //      // 選手情報(object)が返ってきた場合
-  //      const message = arrangeText(obj);
-  //      messages = [
-  //                  {type: 'image', 
-  //                   originalContentUrl: obj.photo_url,
-  //                   previewImageUrl: obj.photo_url},
-  //                  {type: 'text',
-  //                   text: message}
-  //                 ];
-  //    }
-  //  } else {
-  //    // 何も返ってこなかった場合、エラーを返す。
-  //    messages = {type: 'text',
-  //                text: "選手が見つかりませんでした。"};
-  //  }
-  //  // use reply API
-  //  return client.replyMessage(event.replyToken, messages);
-  //}); 
-//}
 
 // listen on port
 const port = process.env.PORT || 3000;
@@ -253,6 +163,21 @@ function arrangeText(obj) {
     res = res + "<今シーズン未出場>"
   }
   return res;
+}
+
+// 選手情報JSONからメッセージオブジェクト作成する関数
+function createMsgObj(obj) {
+  return messages = [
+    { 
+      type: 'image', 
+      originalContentUrl: obj.photo_url,
+      previewImageUrl: obj.photo_url
+    },
+    { 
+      type: 'text',
+      text: arrangeText(obj)
+    }
+  ];
 }
 
 // テキストにチーム名、背番号が含まれるか判定する関数
