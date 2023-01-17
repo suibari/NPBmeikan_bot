c7dd36bdc983b6aacbe04b5adfb4d77e607bc8fb
suibari <404.not.found.address@gmail.com>
2023-01-17 13:54:44 +0900

    lineインスタンス生成していなかったので修正

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET
 };
+const client = new line.Client(config);
 const router    = require('./router.js')
 
 // create Express app
